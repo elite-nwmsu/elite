605f96578cc1fca08ea46cc31eaebec6de989f82
S525906 <S525906@NW33399.nwmissouri.edu>
2017-02-28 23:04:12 -0600

    i have added section class

---
+++ SSS/src/sss/Section.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sss;
+
+/**
+ *
+ * @author ghori
+ */
+public class Section {
+    String id;
+        boolean sw;
+        String next, current, next1, prev1;
+}
